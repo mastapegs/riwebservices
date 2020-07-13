1f83a5f4458d08efa35bcb91b7fe19f98ab7e15c
Matthew Pagan <mastapegs01@gmail.com>
2020-07-13 15:18:00 -0400

    added material ui

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 const Layout = ({ children: page }) => {
   return (
     <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Helmet>
       {page}
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Typography } from '@material-ui/core'
 
 const Index = () => {
   return (
     <>
-      <h1>Hello, World!</h1>
+      <Typography variant='h1'>Hello, World!</Typography>
     </>
   )
 }
