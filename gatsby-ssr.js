2bb3ff36f1170274c1e9766427df8a4f72d680c3
Matthew Pagan <mastapegs01@gmail.com>
2020-07-14 14:18:16 -0400

    added Material UI Theming to the site

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,22 @@
 const React = require("react")
 const Layout = require("./src/components/Layout").default
+const { ThemeProvider } = require('@material-ui/core/styles')
+const { createMuiTheme } = require('@material-ui/core/styles')
+
+const theme = createMuiTheme()
 
 exports.wrapPageElement = ({ element, props }) => {
   return (
     <Layout {...props}>
       {element}
     </Layout>
+  )
+}
+
+exports.wrapRootElement = ({ element }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      {element}
+    </ThemeProvider>
   )
 }
