05f314f91bca3dd5671d84aea429d42597387b65
Matthew Pagan <mastapegs01@gmail.com>
2020-07-17 19:42:37 -0400

    use a container

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Typography, Button } from '@material-ui/core'
+import { Typography, Button, Container } from '@material-ui/core'
 
 const Index = () => {
   return (
     <>
-      <Typography variant='h1'>Hello, World!</Typography>
-      <Button variant='contained' color='primary'>Test Button</Button>
+      <Container>
+        <Typography variant='h1'>Hello, World!</Typography>
+        <Button variant='contained' color='primary'>Test Button</Button>
+      </Container>
     </>
   )
 }
