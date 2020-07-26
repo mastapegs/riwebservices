f4dbdfa47cc93fc3a20fe4d42fff8fb1d7d67571
Matthew Pagan <mastapegs01@gmail.com>
2020-07-26 17:07:02 -0400

    replaced top bar nav with bottom nav

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import MaterialSetup from './MaterialSetup'
 import Header from './Header'
+import BottomNav from './BottomNav'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  main: {
+    marginBottom: theme.spacing(10)
+  }
+}))
 
 const Layout = ({ children: page }) => {
+  const classes = useStyles()
   return (
     <>
       <MaterialSetup />
       <Header />
-      <main>
+      <main className={classes.main}>
         {page}
       </main>
+      <BottomNav />
     </>
   )
 }
