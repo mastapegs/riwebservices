526d8c1e540a8eba88dc9a58881be565551d2a46
Matthew Pagan <mastapegs01@gmail.com>
2020-07-29 12:45:32 -0400

    override theme.js from gatsby-theme-material-ui

---
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from "@material-ui/core";
+// import { blue } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+    // Example of primary color override
+    // palette: {
+    //   primary: blue
+    // },
+});
+
+export default theme;
