1a9b65f54e3f8ac86ec00b6cbe7bd00f9827b508
Matthew Pagan <mastapegs01@gmail.com>
2020-07-29 13:33:19 -0400

    restrict size of table

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,8 +18,20 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
+const useContactData = () => ([
+  {
+    contactType: 'Phone',
+    contactData: '401-332-7084',
+  },
+  {
+    contactType: 'Email',
+    contactData: 'mpagan@riwebservices.com',
+  },
+])
+
 const Contact = () => {
   const classes = useStyles()
+  const contactData = useContactData()
   return (
     <>
       <div className={classes.heroContent}>
@@ -30,40 +42,30 @@ const Contact = () => {
           <Typography variant="h5" align="center" color="textSecondary" paragraph>
             Whether you have questions on how my services can help your business grow, or simply want to talk about your online web presence, it would be my pleasure to speak with you.
           </Typography>
-        </Container>
-        <TableContainer>
-          <Table>
-            <TableHead>
+          <TableContainer>
+            <Table>
+              <TableHead>
 
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>
-                  <Typography>
-                    Phone
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>
-                    {'401-332-7084'}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>
-                  <Typography>
-                    Email
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>
-                    {'mpagan@riwebservices.com'}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {contactData.map(({ contactType, contactData }) => (
+                  <TableRow>
+                    <TableCell>
+                      <Typography>
+                        {contactType}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography>
+                        {contactData}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Container>
       </div>
     </>
   )
