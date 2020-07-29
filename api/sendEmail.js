78d369f57256494517c5242e64b4b8814ad45621
Matthew Pagan <mastapegs01@gmail.com>
2020-07-29 16:38:22 -0400

    customize email sent to me

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -4,9 +4,9 @@ export default async (req, res) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   const msg = {
     to: 'mpagan@riwebservices.com',
-    from: 'mpagan@riwebservices.com',
-    subject: 'Sending with Twilio SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
+    from: req.body.email,
+    subject: `${name} | ${businessName}`,
+    text: req.body.message,
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
   }
   await sgMail.send(msg)
