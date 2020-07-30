0c81c258cc6a70ad8c1aa420e3273854438e9c51
Matthew Pagan <mastapegs01@gmail.com>
2020-07-30 16:04:57 -0400

    more refactoring of sendEmail api

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,39 +1,66 @@
 import sgMail from '@sendgrid/mail'
 
-const send = async req => {
+const send = async ({
+  name,
+  businessName,
+  phone,
+  email,
+  message,
+}) => {
+
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
   const msg = {
     to: 'mpagan@riwebservices.com',
     from: 'mpagan@riwebservices.com',
-    subject: `${req.body.name} | ${req.body.businessName}`,
+    subject: `${name} | ${businessName}`,
     text:
-      `Name: ${req.body.name}\n` +
-      `Business Name: ${req.body.businessName}\n` +
-      `Phone: ${req.body.phone}\n` +
-      `Email: ${req.body.email}\n` +
-      `Message: ${req.body.message}`,
+      `Name: ${name}\n` +
+      `Business Name: ${businessName}\n` +
+      `Phone: ${phone}\n` +
+      `Email: ${email}\n` +
+      `Message: ${message}`,
     html:
-      `<strong>Name:</strong> ${req.body.name}<br>` +
-      `<strong>Business Name:</strong> ${req.body.businessName}<br>` +
-      `<strong>Phone:</strong> ${req.body.phone}<br>` +
-      `<strong>Email:</strong>: ${req.body.email}<br>` +
-      `<strong>Message:</strong> ${req.body.message}<br>`
+      `<strong>Name:</strong> ${name}<br>` +
+      `<strong>Business Name:</strong> ${businessName}<br>` +
+      `<strong>Phone:</strong> ${phone}<br>` +
+      `<strong>Email:</strong>: ${email}<br>` +
+      `<strong>Message:</strong> ${message}<br>`
   }
+
   let emailSuccess = true
   await sgMail.send(msg).catch(error => {
+    console.log(error)
     emailSuccess = false
   })
   return emailSuccess
+
 }
 
 const sendEmail = async (req, res) => {
 
-  const emailSuccess = await send(req)
-  
+  const {
+    name,
+    businessName,
+    phone,
+    email,
+    message,
+  } = req.body
+
+  let emailSuccess = false
+  emailSuccess = await send({
+    name,
+    businessName,
+    phone,
+    email,
+    message,
+  })
+
   res.json({
     data: req.body,
     emailSuccess
   })
+
 }
 
 export default sendEmail
