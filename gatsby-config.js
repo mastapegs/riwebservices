8248f0d6146de975e2f3237d0472af97eae8004f
Matthew Pagan <mastapegs01@gmail.com>
2020-08-04 10:08:09 -0400

    manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,17 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-theme-material-ui',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `RIWebServices`,
+        short_name: `RIWebServices`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/favicon/favicon.png`,
+      },
+    },
   ],
 }
