0cd58c6f1c25867bb610f0e44d1b3df005481f36
Matthew Pagan <mastapegs01@gmail.com>
2020-08-05 15:33:42 -0400

    refactor out tiers data

---
+++ src/data/pricingTiers.js
@@ -0,0 +1,42 @@
+const teirs = [
+  {
+    title: 'Multi-Page Website w/ Contact Form',
+    price: '1000',
+    monthlyPrice: '30',
+    description: [
+      'Multiple Pages',
+      'Offline Capability',
+      'Contact Form linked to your Email and Phone',
+    ],
+    buttonText: 'Make yourself seen',
+    buttonVariant: 'outlined',
+  },
+  {
+    title: 'Website + Blog',
+    subheader: 'Most popular',
+    price: '3000',
+    monthlyPrice: '60',
+    description: [
+      'Everything included in a Multi-Page Site w/ Contact Form',
+      'A blog (or more than one!) with the ability to make as many new posts as you\'d like',
+      'Make your website editable for the whole team',
+    ],
+    buttonText: 'Stand out amongst the crowd',
+    buttonVariant: 'contained',
+  },
+  {
+    title: 'E-Commerce Web Application',
+    price: '5000',
+    monthlyPrice: '120',
+    description: [
+      'Everything included in a Website + Blog',
+      'Shopify Integration',
+      'Sell your products and services to customers online',
+      'Create a dropshipping service and sell branded merchandise',
+    ],
+    buttonText: 'Take your business to the next level',
+    buttonVariant: 'outlined',
+  },
+]
+
+export default teirs
