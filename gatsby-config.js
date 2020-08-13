d429614d2d86c6ec65330a951c2e7a50052dc0f2
Matthew Pagan <mastapegs01@gmail.com>
2020-08-13 14:49:52 -0400

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,14 @@ module.exports = {
       },
     },
     'gatsby-plugin-sitemap',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-175299660-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+      },
+    },
   ],
 }
