c8c88d46e8b14753850113f1a6011bf72ed7a219
Matthew Pagan <mastapegs01@gmail.com>
2020-08-18 12:00:55 -0400

    Add fade page transition to individual blog posts

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Fade } from '@material-ui/core'
 
 const BlogPost = () => {
+  const [fadeIn, setFadeIn] = useState(false)
+  useEffect(() => {
+    setFadeIn(true)
+    return (() => {
+      setFadeIn(false)
+    })
+  }, [fadeIn])
   return (
     <>
-      <h1>This is a blog post</h1>
+      <Fade in={fadeIn}>
+        <h1>This is a blog post</h1>
+      </Fade>
     </>
   )
 }
