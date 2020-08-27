5642d9c25ff1f5af897485dbc4d1dadbb98d42cf
Matthew Pagan <mastapegs01@gmail.com>
2020-08-27 10:44:18 -0400

    Used Material Grids and Cards to show Products

--- src/pages/shopify-test.js
+++ src/pages/shopify-test.js
@@ -1,35 +1,71 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import { Container } from '@material-ui/core'
+import {
+  Container,
+  Button,
+  Card,
+  CardContent,
+  CardActions,
+  Grid,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  gridItem: {
+    margin: theme.spacing(2, 0),
+  },
+  gridContainer: {
+    flexGrow: 1,
+  }
+}))
 
 const ShopifyTest = ({ data }) => {
+  const classes = useStyles()
   return (
     <>
       <Container>
-        {((data) => {
-          const products = data.allShopifyProduct.edges
-          return (
-            products.map(({ node }) => {
-              const {
-                id,
-                title,
-                description,
-                priceRange: {
-                  minVariantPrice: {
-                    amount: price
+        <Grid container className={classes.gridContainer} spacing={2}>
+          {((data) => {
+            const products = data.allShopifyProduct.edges
+            return (
+              products.map(({ node }) => {
+                const {
+                  id,
+                  title,
+                  description,
+                  priceRange: {
+                    minVariantPrice: {
+                      amount: price
+                    }
                   }
-                }
-              } = node
-              return (
-                <div key={id}>
-                  <h2>{title}</h2>
-                  <p>{description}</p>
-                  <p>${parseInt(price).toFixed(2)}</p>
-                </div>
-              )
-            })
-          )
-        })(data)}
+                } = node
+                return (
+                  <Grid item xs={12} sm={6} md={4} className={classes.gridItem}>
+                    <Card key={id}>
+                      <CardContent>
+                        <h2>{title}</h2>
+                        <p>{description}</p>
+                        <p>${parseInt(price).toFixed(2)}</p>
+
+                      </CardContent>
+                      <CardActions>
+                        <Button
+                          variant='contained'
+                          color='primary'
+                          onClick={() => {
+                            console.log(`Product: ${title} added to cart.`)
+                          }}
+                        >
+                          {'Add to Cart'}
+                        </Button>
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                )
+              })
+            )
+          })(data)}
+        </Grid>
       </Container>
     </>
   )
