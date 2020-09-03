0862f8733cd69b2f1a83f0a9dc68d6e83b93258b
Matthew Pagan <mastapegs01@gmail.com>
2020-09-03 14:47:04 -0400

    leverage GraphQL fragments

--- src/queries/shopifyCartQueries.js
+++ src/queries/shopifyCartQueries.js
@@ -1,23 +1,30 @@
 import { gql } from '@apollo/client'
 
-export const CREATE_CHECKOUT = gql`
-  mutation CreateCheckout {
-    checkoutCreate(input: {}) {
-      checkout {
-        id
-        webUrl
-        lineItems(first: 100) {
-          edges {
-            node {
-              id
-              quantity
-              title
-            }
-          }
+const CHECKOUT_FRAGMENT = gql`
+  fragment CheckoutFragment on Checkout {
+    id
+    webUrl
+    lineItems(first: 100) {
+      edges {
+        node {
+          id
+          quantity
+          title
         }
       }
     }
   }
+`
+
+export const CREATE_CHECKOUT = gql`
+  mutation CreateCheckout {
+    checkoutCreate(input: {}) {
+      checkout {
+        ...CheckoutFragment
+      }
+    }
+  }
+  ${CHECKOUT_FRAGMENT}
 `
 
 export const GET_CHECKOUT = gql`
@@ -25,58 +32,31 @@ export const GET_CHECKOUT = gql`
     node(id: $id) {
       id
       ... on Checkout {
-        id
-        webUrl
-        lineItems(first: 100) {
-          edges {
-            node {
-              id
-              quantity
-              title
-            }
-          }
-        }
+        ...CheckoutFragment
       }
     }
   }
+  ${CHECKOUT_FRAGMENT}
 `
 
 export const ADD_LINE = gql`
   mutation ($id: ID!, $variantID: ID!, $quantity: Int!) {
     checkoutLineItemsAdd(checkoutId: $id, lineItems: {quantity: $quantity, variantId: $variantID}) {
       checkout {
-        id
-        webUrl
-        lineItems(first: 100) {
-          edges {
-            node {
-              id
-              quantity
-              title
-            }
-          }
-        }
+        ...CheckoutFragment
       }
     }
   }
+  ${CHECKOUT_FRAGMENT}
 `
 
 export const EMPTY_CART = gql`
   mutation ($id: ID!) {
     checkoutLineItemsReplace(checkoutId: $id, lineItems: []) {
       checkout {
-        id
-        webUrl
-        lineItems(first: 100) {
-          edges {
-            node {
-              id
-              quantity
-              title
-            }
-          }
-        }
+        ...CheckoutFragment
       }
     }
   }
+  ${CHECKOUT_FRAGMENT}
 `
