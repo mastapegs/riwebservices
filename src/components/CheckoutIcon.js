588cad09cb87ac8d4d5d8736cda0e213d4b70710
Matthew Pagan <mastapegs01@gmail.com>
2020-09-09 13:32:48 -0400

    adding cart modal

--- src/components/CheckoutIcon.js
+++ src/components/CheckoutIcon.js
@@ -1,13 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
-import { IconButton, Badge } from '@material-ui/core'
+import { IconButton, Badge, Dialog, DialogTitle, Grid, Card, Container } from '@material-ui/core'
 import ShopifyContext from '../contexts/ShopifyContext'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    padding: theme.spacing(0, 2)
+  }
+}))
 
 const Component = () => {
+  const [open, setOpen] = useState(false)
   const { checkout } = useContext(ShopifyContext)
+  const classes = useStyles()
+  const handleClose = () => {
+    setOpen(false)
+  }
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
   return (
     <>
-      <IconButton color='inherit'>
+      <IconButton color='inherit' onClick={handleClickOpen}>
         <Badge badgeContent={(() => {
           if (!checkout) return 0
           let sum = 0
@@ -19,6 +34,24 @@ const Component = () => {
           <ShoppingCartIcon />
         </Badge>
       </IconButton>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>Shopping Cart</DialogTitle>
+        <Container>
+          <Grid container spacing={3}>
+            {checkout?.lineItems.edges.map(({ node: { id, quantity, title } }) => (
+              <Grid item xs={12} key={id}>
+                <Card className={classes.card}>
+                  <p>{`${title}`}</p>
+                  <p>{`Quantity: ${quantity}`}</p>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </Dialog>
     </>
   )
 }
