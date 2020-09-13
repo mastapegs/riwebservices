9b68b40aa7ecedd65488498b6c8832934cd0efb2
Matthew Pagan <mastapegs01@gmail.com>
2020-09-12 20:28:57 -0400

    removed apollo provider preshopify example (countries)

--- src/components/RootWrapper.js
+++ src/components/RootWrapper.js
@@ -1,14 +1,10 @@
 const React = require('react')
-const { ApolloProvider } = require('@apollo/client')
-const countriesClient = require('../clients/countriesClient')
 const ShopifyContextProvider = require('../providers/ShopifyContextProvider').default
 
 const RootWrapper = ({ element }) => {
   return (
     <ShopifyContextProvider>
-      <ApolloProvider client={countriesClient}>
-        {element}
-      </ApolloProvider>
+      {element}
     </ShopifyContextProvider >
   )
 
