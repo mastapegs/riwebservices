6a8f66d42b3139428dd6a2c2ac1e14f34fecd9e2
Matthew Pagan <mastapegs01@gmail.com>
2020-09-17 12:57:02 -0400

    added auth0 provider to gatsby

--- src/components/RootWrapper.js
+++ src/components/RootWrapper.js
@@ -1,11 +1,14 @@
 const React = require('react')
 const ShopifyContextProvider = require('../providers/ShopifyContextProvider').default
+const Auth0Provider = require('../providers/Auth0Provider').default
 
 const RootWrapper = ({ element }) => {
   return (
-    <ShopifyContextProvider>
-      {element}
-    </ShopifyContextProvider >
+    <Auth0Provider>
+      <ShopifyContextProvider>
+        {element}
+      </ShopifyContextProvider >
+    </Auth0Provider>
   )
 
 }
---
+++ src/providers/Auth0Provider.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const CustomAuth0Provider = ({ children }) => {
+  return (
+    <Auth0Provider
+      domain="riwebservices.us.auth0.com"
+      clientId="x38koCXqGcJcfZTCEc0aANFbe8FSDe5z"
+      redirectUri={window.location.origin}
+    >
+      {children}
+    </Auth0Provider>
+  )
+}
+
+export default CustomAuth0Provider
